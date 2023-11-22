c048105d5685478e58cb1f6af280e7f47a2c06cd
tiagolofi <jejeco75@gmail.com>
2023-11-22 01:07:33 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,86 @@
+
+import streamlit as st
+import yt
+
+st.set_page_config(
+	page_title = 'JecoTube - o seu YouTube sem anúncios',
+	layout = 'wide',
+	initial_sidebar_state = 'collapsed' 
+)
+
+st.header('JecoTube - o seu YouTube sem anúncios', divider = 'red')
+
+st.session_state['ultima_busca'] = "Notícias em Alta no Brasil"
+
+col1, col2 = st.columns(2)
+
+with col1:
+
+    st.subheader('Acesso com Link')
+
+    link = st.text_input('URL do Vídeo:', value = 'https://www.youtube.com/watch?v=khplMpm4ctc&ab_channel=Ado')
+
+    with st.expander('Mostrar/Ocultar Vìdeo'):
+
+        st.markdown(
+            yt.embed_link(
+                hash_video = yt.get_hash_video(link)
+            ),
+            unsafe_allow_html = True
+        )
+
+with col2:
+    
+    st.subheader('Acesso com Hash')
+
+    hash_ = st.text_input('Hash do Vídeo:')
+
+    with st.expander('Mostrar/Ocultar Vìdeo '):
+
+        st.markdown(
+            yt.embed_link(
+                hash_video = hash_
+            ),
+            unsafe_allow_html = True
+        )
+
+st.subheader('Pesquisa de Vídeo')
+
+termo = st.text_input('Termo de Pesquisa:')
+botao = st.button('Pesquisar')
+
+if botao:
+    st.session_state['ultima_busca'] = termo
+
+resultados = yt.get_data_query(search_term = st.session_state['ultima_busca'])
+
+lines = len(resultados) // 5
+
+c1, c2, c3, c4, c5 = st.columns(5)
+
+WIDTH = 200
+
+with c1:
+
+    for i in resultados[0:lines]:
+        st.image(i['thumb'], caption = i['title'] + ' - ' + i['hash_video'], width = WIDTH)
+
+with c2:
+
+    for i in resultados[lines + 1:lines * 2]:
+        st.image(i['thumb'], caption = i['title'] + ' - ' + i['hash_video'], width = WIDTH)
+
+with c3:
+
+    for i in resultados[(lines * 2) + 1:lines * 3]:
+        st.image(i['thumb'], caption = i['title'] + ' - ' + i['hash_video'], width = WIDTH)
+
+with c4:
+
+    for i in resultados[(lines * 3) + 1:lines * 4]:
+        st.image(i['thumb'], caption = i['title'] + ' - ' + i['hash_video'], width = WIDTH)
+
+with c5:
+
+    for i in resultados[(lines * 4) + 1:len(resultados)]:
+        st.image(i['thumb'], caption = i['title'] + ' - ' + i['hash_video'], width = WIDTH) 
---
+++ yt.py
@@ -0,0 +1,47 @@
+
+import requests
+import re
+import json
+
+def get_hash_video(url: str) -> str:
+
+    if '&' in url:
+        return re.search(r'=(.*?)&', url).group(1)
+    else:
+        return re.search(r'=(.*?)&', url + '&').group(1)
+
+def embed_link(hash_video: str) -> str:
+
+    return f'''
+    <iframe
+        width="100%" height="400" src="https://www.youtube-nocookie.com/embed/{hash_video}"
+		frameborder="0" allow="autoplay; encrypted-media" allowfullscreen> 
+    ></iframe>
+    '''
+
+def get_data_query(search_term: str) -> list:
+
+    url_query = f'''https://www.youtube.com/results?search_query={search_term}'''
+
+    session = requests.get(
+        url_query, 
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
+        }
+    )
+
+    data_unestrutured = re.findall(r'''{"videoRenderer":(.*?),"longBylineText''', session.text)
+
+    list_struc = []
+
+    for i in data_unestrutured:
+        data_un = json.loads(i + '}')
+        list_struc.append(
+            {
+                'hash_video': data_un['videoId'],
+                'thumb': data_un['thumbnail']['thumbnails'][0]['url'],
+                'title': data_un['title']['runs'][0]['text']
+            }
+        )
+
+    return list_struc
