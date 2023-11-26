87f5e304ff08f594764df285220dba581e7f9112
Tiago Matos <69057723+tiagolofi@users.noreply.github.com>
2023-11-26 12:32:08 -0300

    Update yt.py

--- yt.py
+++ yt.py
@@ -26,7 +26,7 @@ def embed_link_minimal(hash_video: str) -> str:
 
     return f'''
     <iframe
-        width="100%" height="100" src="https://www.youtube-nocookie.com/embed/{hash_video}"
+        width="100%" height="150" src="https://www.youtube-nocookie.com/embed/{hash_video}"
 	frameborder="0" allow="autoplay; encrypted-media" allowfullscreen> 
     ></iframe>
     '''
